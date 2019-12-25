ae27b6a95de30189934eae3be7a97137aea68477
lolololololo6 <58217657+lolololololo6@users.noreply.github.com>
2019-12-25 10:03:53 +0100

    Final solution.
    Works for undirected graph and directed

--- Traccia3.py
+++ Traccia3.py
@@ -1,22 +1,22 @@
 def DFS(g,u):
-
-    print(u)
-    u._pre=u
-    tmp=u
-    for e in g.incident_edges(u):
-        if(e._destination != u and e._destination._pre == None): #opposite al posto di destination
-            tmp = e._destination
-            tmp._pre = e._origin
-            print(tmp._element)
+    nodes = []
+    nodes.append(u)
+    for e in g.incident_edges(u, True):
+        if(e.opposite(u)._pre==None):
+            tmp = e.opposite(u)
+            tmp._pre = u
+            nodes.append(tmp)
             i=0
             while(i < len(list(g.incident_edges(tmp)))):
-                if(list(g.incident_edges(tmp))[i]._destination != tmp and list(g.incident_edges(tmp))[i]._destination._pre == None):
-                    list(g.incident_edges(tmp))[i]._destination._pre = tmp
-                    print(list(g.incident_edges(tmp))[i]._destination._element)
-                    tmp = list(g.incident_edges(tmp))[i]._destination
+                if(list(g.incident_edges(tmp))[i].opposite(tmp)._pre == None and list(g.incident_edges(tmp))[i].opposite(tmp)!=u):
+                    list(g.incident_edges(tmp))[i].opposite(tmp)._pre = tmp
+                    tmp = list(g.incident_edges(tmp))[i].opposite(tmp)
+                    nodes.append(tmp)
                     i=0
                 else:
                     i=i+1
                     if(i == len(list(g.incident_edges(tmp))) and tmp._pre!=u):
                         tmp= tmp._pre
                         i=0
+    for v in nodes:
+        print(v._element)
